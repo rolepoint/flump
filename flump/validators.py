3ca9cfd7fa704d573a669e119acfa487c5a1862a
Carl Henderson <carl.s.henderson@gmail.com>
2015-11-17 14:56:09 +0000

    Add Immutable Validator
    Ensure that a field cannot be updated through a `Patch` request.

---
+++ flump/validators.py
@@ -0,0 +1,17 @@
+from flask import request
+
+from marshmallow.exceptions import ValidationError
+from marshmallow.validate import Validator
+
+
+class Immutable(Validator):
+    """
+    Validator which ensures that a field cannot be updated through a `PATCH`
+    request.
+    """
+
+    def __call__(self, value):
+        if request.method == 'PATCH':
+            raise ValidationError('Immutable.')
+
+        return value
